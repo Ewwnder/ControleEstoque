a6fec66e1908c79d8fd9680244a4599f4769ea40
Nicolas Ferreira <164695775+Ewwnder@users.noreply.github.com>
2025-10-18 14:27:44 -0300

    fix: corrige arquivos apagados

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -7,8 +7,62 @@ package model;
 
 /**
  *
- * @author User
+ * @author Nicolas Ap
  */
 public class Produto {
     
+    private int id;
+    private String nome;
+    private int idCategoria;
+    private int quantidade;
+    private double preco;
+
+    public Produto(int id, String nome, int idCategoria, int quantidade, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.idCategoria = idCategoria;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
+
