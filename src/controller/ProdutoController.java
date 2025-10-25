849054daf920ff3f3dc9c3673b993402e1bc7288
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-10-25 17:20:50 -0300

    feat: adiciona ProdutoController

---
+++ src/controller/ProdutoController.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.util.List;
+import model.Produto;
+import service.ProdutoService;
+
+/**
+ *
+ * @author Nicolas Ap
+ */
+public class ProdutoController {
+    
+    private final ProdutoService produtoService;
+    
+    public ProdutoController(){
+        this.produtoService = new ProdutoService();
+    }
+    
+    public void adicionarProduto(String nome, int idCategoria, int quantidade, double preco){
+        Produto produto = new Produto(0, nome, idCategoria, quantidade, preco);
+        produtoService.adicionarProduto(produto);
+        System.out.println("Produto adicionado com sucesso no sistema!");
+    }
+    
+    public void listarProdutos(){
+        List<Produto> produtos = produtoService.listarProdutos();
+        produtos.forEach(p -> System.out.println(p.getId() + " - " + p.getNome() + " | " + p.getQuantidade() + " unidades | R$ " + p.getPreco()));
+    }
+    
+    public void removerProduto(int id){
+        boolean removeu = produtoService.removerProduto(id);
+        System.out.println(removeu ? "Produto removido com sucesso!" : "Produto não encontrado!");
+    }
+    
+    public void editarProduto(int id, String nome, int idCategoria, int quantidade, double preco){
+        Produto produto = new Produto(id, nome, idCategoria, quantidade, preco);
+        boolean atualizou = produtoService.editarProduto(produto);
+        System.out.println(atualizou ? "Produto atualizado com sucesso!" : "Produto não encontrado!");
+    }
+    
+    public void buscarProdutoId(int id){
+        try{
+            Produto produto = produtoService.buscarProdutoId(id);
+            System.out.println("Produto com o ID informado: " + produto.getNome());
+        } catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void buscarProdutoNome(String nome){
+        try{
+            Produto produto = produtoService.buscarProdutoNome(nome);
+            System.out.println("Produto com o nome informado: " + produto.getNome());
+        } catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
